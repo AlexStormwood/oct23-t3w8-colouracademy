67f9874b4314f7d2f3375df3128f5be89a63c5d8
Alex H <alex.holder@bigfootds.com>
2024-05-23 20:29:27 +1000

    feat: theme context created but not used

---
+++ src/contexts/currentThemeContext.js
@@ -0,0 +1,41 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useLocalStorage } from "react-use";
+
+
+export const CurrentThemeDataContext = createContext([]);
+export const CurrentThemeDispatchContext = createContext(null);
+
+export function useCurrentThemeData(){
+	return useContext(CurrentThemeDataContext);
+}
+
+export function useCurrentThemeDispatch(){
+	return useContext(CurrentThemeDispatchContext);
+}
+
+export function CurrentThemeProvider({children}){
+	let [currentTheme, setCurrentTheme] = useState([]);
+
+	let [currentThemeLocalStorage, setCurrentThemeLocalStorage] = useLocalStorage("css-currenttheme", "#000000");
+
+	useEffect(() => {
+		// On component load, read from localstorage and set it to state 
+		setCurrentTheme(currentThemeLocalStorage);
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	useEffect(() => {
+		// When baseColour updates, write the new value to localstorage 
+		setCurrentThemeLocalStorage(currentTheme);
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [currentTheme]);
+
+	return (
+		<CurrentThemeDataContext.Provider value={currentTheme}>
+			<CurrentThemeDispatchContext.Provider value={setCurrentTheme}>
+				{children}
+			</CurrentThemeDispatchContext.Provider>
+		</CurrentThemeDataContext.Provider>
+	)	
+
+}
