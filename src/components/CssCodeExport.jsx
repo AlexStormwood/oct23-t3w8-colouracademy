44fec3dc4f491d0ea0f8101526dd65da404462fb
Alex H <alex.holder@bigfootds.com>
2024-05-25 09:53:46 +1000

    feat: code is copyable to clipboard

--- src/components/CssCodeExport.jsx
+++ src/components/CssCodeExport.jsx
@@ -12,7 +12,7 @@ export function CssCodeExport(){
 		codeAsString += `:root {\n`;
 
 		currentTheme.colours.forEach(colourObj => {
-			codeAsString += `\t--${colourObj.themeName}: ${colourObj.hex};\n`
+			codeAsString += `\t--${colourObj.themeName}-${colourObj.strength}: ${colourObj.hex};\n`
 		})
 
 		codeAsString += `}`;
@@ -27,6 +27,9 @@ export function CssCodeExport(){
 				{buildCssVariableString()}
 			</SyntaxHighlighter>
 
+			<button onClick={() => navigator.clipboard.writeText(buildCssVariableString())}>
+				Copy code to clipboard
+			</button>
 		</div>
 	)
 }
